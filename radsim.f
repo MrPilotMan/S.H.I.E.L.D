a676de071fc16ba8f0f6c16ec7303d87f716a57b
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-10-13 21:14:45 -0400

    Update radsim.f

--- radsim.f
+++ radsim.f
@@ -1,4 +1,10 @@
 program radsim
 implicit none
 
+real :: iterations = 100
+real :: innerRadius = 10                            ! m
+real :: torusRadius = 20                            ! m
+real :: totalRadius = innerRadius + torusRadius.    ! m
+
+
 end program radsim
