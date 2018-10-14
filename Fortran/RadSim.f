fb13cb7986af522c0979531e5bd1971bdceb5936
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-10-13 21:26:35 -0400

    struct

---
+++ Fortran/RadSim.f
@@ -0,0 +1,22 @@
+program radsim
+implicit none
+
+! Constants
+real :: particles     = 100
+real :: hits          = 0
+real :: iterations    = 100
+real :: i             = .1                            ! A
+
+real :: innerRadius   = 10                            ! meters
+real :: torusRadius   = 20                            ! meters
+real :: totalRadius   = innerRadius + torusRadius     ! meters
+
+real :: pi            = 4 * atan(1.0_8)
+real :: mu            = 4 * pi * (10 ** -7);          ! [Tm/A]
+
+real :: dTheta        = .001 / pi                     ! radians
+real :: delt          = 10 ** -6                      ! seconds
+
+real :: scale         = 100000
+
+end program radsim
