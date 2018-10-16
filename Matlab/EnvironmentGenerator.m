e72083e386ad27bb07c56a76c30a39b81ad49c6b
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-10-15 20:15:04 -0400

    Update Matlab/EnvironmentGenerator.m

--- Matlab/EnvironmentGenerator.m
+++ Matlab/EnvironmentGenerator.m
@@ -1,65 +1,63 @@
 function environments = Radiation_Environment_Generator(particles)
-        for o = 1:particles
-            charge = randi([-3, 3]);
-            mass = rand * 3.952562528e-25;
-            
-            px1 = (2 * 100 * rand) - 100;
-            py1 = (2 * 100 * rand) - 100;
-            pz1 = (2 * 100 * rand) - 100;
-            p = [px1, py1, pz1];
-            
-            vx1 = (2 * 3e8 * rand) - 3e8;
-            vy1 = (2 * 3e8 * rand) - 3e8;
-            vz1 = (2 * 3e8 * rand) - 3e8;
-            
-            while (abs(tan(acos(dot([vx1, vy1, vz1], [-px1, -py1, -pz1]) / (norm([vx1, vy1, vz1]) * norm([px1, py1, pz1])))))) > abs((30 / norm([px1, py1, pz1])))
-                vx1 = (2 * 3e8 * rand) - 3e8;
-                vy1 = (2 * 3e8 * rand) - 3e8;
-                vz1 = (2 * 3e8 * rand) - 3e8;
-                while sqrt((vx1^2) + (vy1^2) + (vz1^2)) >= 300000000
-                    vx1 = (2 * 3e8 * rand) - 3e8;
-                    vy1 = (2 * 3e8 * rand) - 3e8;
-                    vz1 = (2 * 3e8 * rand) - 3e8;
+
+    for o = 1:particles
+        charge = randi([-3, 3]);
+        mass = rand * 3.952562528e-25;
+
+        positionVector = updateVector('p');
+        velocityVector = updateVector("v");
+
+        while (abs(tan(acos(dot(velocityVector, -positionVector) / (norm(velocityVector) * norm(positionVector)))))) > abs((30 / norm(positionVector)))
+            velocityVector = updateVector("v");
+
+            while sqrt((velocityVector(1)^2) + (velocityVector(2)^2) + (velocityVector(3)^2)) >= 300000000
+                velocityVector = updateVector("v");
+            end
         end
+
+        % if px > 0 && vx > 0 || px < 0 && vx < 0
+        %   vx = vx * (-1);
+        % end
+
+        % if py > 0 && vy > 0 || py < 0 && vy < 0
+        %   vy *= -1;
+        % end
+
+        % if pz > 0 && vz > 0 || pz < 0 && vz < 0
+        %   vz *= -1;
+        % end
+
+        accelerationVector = [0, 0, 0];
+
+        line = [mass, charge, ...
+                positionVector(1), positionVector(2), positionVector(3), ...
+                velocityVector(1), velocityVector(2), velocityVector(3), ...
+                accelerationVector(1), accelerationVector(2), accelerationVector(3)];
+
+        environment(o, :) = line;
     end
-    
-    v = [vx1, vy1, vz1];
-    
-%     if px > 0 && vx > 0
-%         vx = vx * (-1);
-%     end
+    environments = environment;
+end
 
-%     if px < 0 && vx < 0
-%         vx = vx * (-1);
-%     end
+function vector = updateVector(algorithmType)
+        vector = [0, 0, 0];
 
-%     if py > 0 && vy > 0
-%         vy = vy * (-1);
-%     end
+        for i = 1:3
+            p_algo = (2 * 100 * rand) - 100;
+            v_algo = (2 * 3e8 * rand) - 3e8;
+            a_algo = (2 * 10000 * rand) - 10000;
 
-%     if py < 0 && vy < 0
-%         vy = vy * (-1);
-%     end
+            % May be a way to eliminate these checks by passing in algo name at function call
+            if algorithmType == "p"
+                value = p_algo;
+            elseif algorithmType == "v"
+                value = v_algo;
+            elseif algorithmType == "a"
+                value = a_algo
+            else
+                fprintf('incorrect alogirthm type');
+            end
 
-%     if pz > 0 && vz > 0
-%         vz = vz * (-1);
-%     end
-
-%     if pz < 0 && vz < 0
-%         vz = vz * (-1);
-%     end
-    
-%     ax = (2 * 10000 * rand) - 10000;
-%     ay = (2 * 10000 * rand) - 10000;
-%     az = (2 * 10000 * rand) - 10000;
-
-        ax1 = 0;
-        ay1 = 0;
-        az1 = 0;
-    a = [ax1, ay1, az1]; 
-    
-    line = [mass, charge, px1, py1, pz1, vx1, vy1, vz1, ax1, ay1, az1];
-    environment(o, :) = line;
+            vector(i) = value;
         end
-        environments = environment;
     end
