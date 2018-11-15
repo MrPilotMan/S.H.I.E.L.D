237125b4a6481b19fa0b596a218cd834de9de436
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-11-14 19:43:02 -0500

    main parallel control flow

--- Matlab/RadSim.m
+++ Matlab/RadSim.m
@@ -14,28 +14,46 @@ torusRadius        = 20;   % meters
 %hits               = 0;
 %misses             = 0;
 
+%parallel           = false;
 %runLetter          = 'a';
 allTocs            = 0;
 
 % Comment out if loading a pregenerated wireGeometry
 % wireGeometry = generateWireGeometry(innerRadius, torusRadius);
 % Read in wireGeometry from .mat file
-% load('wireGeometry.mat');
+ load('wireGeometry/1e4.mat');
 
-while particlesSimualted < particlesRequested
-	tic
-	
-	fprintf('\nStarting simulation: %3.0f \nTotal simulation time: %7.3f seconds \n', uint8(particlesSimualted + 1), allTocs)
-	
-	particleSimulation = simulateParticle(wireGeometry, delta, scale);
-	particlesSimualted = particlesSimualted + 1;
-    
-    %fileName = [runLetter num2str(particlesSimualted) '-particleMatrix.txt'];
-    %csvwrite(fileName, particleSimulation)
+if parallel == false
+    while particlesSimualted < particlesRequested
+        tic
 
-	thisToc = toc
-    allTocs = allTocs + thisToc;
+        fprintf('\nStarting simulation: %3.0f \nTotal simulation time: %7.3f seconds \n', uint8(particlesSimualted + 1), allTocs)
+
+        particleSimulation = simulateParticle(wireGeometry, delta, scale);
+        particlesSimualted = particlesSimualted + 1;
+
+        %fileName = [runLetter num2str(particlesSimualted) '-particleMatrix.txt'];
+        %csvwrite(fileName, particleSimulation)
+
+        thisToc = toc
+        allTocs = allTocs + thisToc;
+    end
+
+    averageToc = allTocs ./ particlesRequested
+    fprintf('Average particle simulation time: %7.3f seconds', averageToc)
+else
+    tic
+    parfor sim = 0:particlesRequested
+        %workerTic = tic
+
+        fprintf('\nStarting simulation: %3.0f \nTotal simulation time: %7.3f seconds \n', uint8(sim + 1))
+
+        particleSimulation = simulateParticle(wireGeometry, delta, scale);
+
+        %fileName = [runLetter num2str(particlesSimualted) '-particleMatrix.txt'];
+        %csvwrite(fileName, particleSimulation)
+
+        %workerToc = toc
+    end
+    toc
 end
-
-averageToc = allTocs ./ particlesRequested
-fprintf('Average particle simulation time: %7.3f seconds', averageToc)
