1d67aeda3bfe73bfc3c57c76a7284f74da701423
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-11-24 16:53:00 -0500

    add check for particle in habitable area

--- Matlab/checkHit.m
+++ Matlab/checkHit.m
@@ -1,14 +1,18 @@
 function hit = checkHit(position, lastPosition)
     hit = 0;
 
-    if all(sign(position ./ lastPosition) >= 0) == false
-
+    inHabitable = any([abs(position) <= [10, 10, 10], abs(lastPosition) <= [10, 10, 10]]);
+    
+    if inHabitable == true
+        hit = 1;
+    elseif all(sign(position ./ lastPosition) >= 0) == false
        %Define particle trajectory line
        parallelVector = position - lastPosition;
        syms t checkposition
        S = solve([checkposition == (parallelVector * t) + lastPosition, abs(checkposition) <= [10,10,10]]);
+       syms t checkPosition;
+       S = solve([checkPosition == (parallelVector * t) + lastPosition, abs(checkPosition) <= [10, 10, 10]]);
        
-       if isempty(S.checkposition) == false
            hit = 1;
        end
     end
